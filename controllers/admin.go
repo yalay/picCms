9ce9690209e1429dd80566f834a9dad9cd3d2daf
yalay <ya_lay@163.com>
2017-05-22 18:40:40 +0800

    feat:添加系统管理配置

--- controllers/admin.go
+++ controllers/admin.go
@@ -13,10 +13,12 @@ var AdminServer  http.Handler
 
 func InitAdmin(adminPath string) {
 	Admin = admin.New(&qor.Config{DB: DB})
-	Admin.AddResource(&models.Article{})
-	Admin.AddResource(&models.Attach{})
-	Admin.AddResource(&models.Cate{})
-	Admin.AddResource(&models.Tags{})
+	Admin.AddResource(&models.Config{}, &admin.Config{Menu: []string{"系统配置"}})
+
+	Admin.AddResource(&models.Article{}, &admin.Config{Menu: []string{"资源管理"}})
+	Admin.AddResource(&models.Attach{}, &admin.Config{Menu: []string{"资源管理"}})
+	Admin.AddResource(&models.Cate{}, &admin.Config{Menu: []string{"资源管理"}})
+	Admin.AddResource(&models.Tags{}, &admin.Config{Menu: []string{"资源管理"}})
 
 	AdminServer = Admin.NewServeMux(adminPath)
 }
