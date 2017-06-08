bae3c80a7ae59302bc8fe781c1cffcc7ddfb811f
yalay <ya_lay@163.com>
2017-06-08 22:07:17 +0800

    fix:没有主键容易有问题

--- models/lang.go
+++ models/lang.go
@@ -1,6 +1,7 @@
 package models
 
 type Lang struct {
+	Id  int32
 	Zh  string `gorm:"type:varchar(250);not null;index"`
 	Zht string `gorm:"type:varchar(250);not null;index"`
 	Eng string `gorm:"type:varchar(250);not null;index"`
